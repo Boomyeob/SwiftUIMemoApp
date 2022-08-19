43a2a5c398a5670a0a7d56593f20f0915fb9e1bd
Boom <65594327+Boomyeob@users.noreply.github.com>
2022-08-20 00:22:21 +0900

    Add files via upload
    iOS 메모 앱

---
+++ SwiftUIMemo/Shared/SwiftUIMemoApp.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftUIMemoApp.swift
+//  Shared
+//
+//  Created by 권순엽 on 2022/08/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMemoApp: App {
+    @StateObject var store = MemoStore()
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            MainListView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(store)
+        }
+    }
+}
---
+++ SwiftUIMemo/Shared/View/MainListView.swift
@@ -0,0 +1,52 @@
+//
+//  MainListView.swift
+//  SwiftUIMemo
+//
+//  Created by 권순엽 on 2022/08/19.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    @EnvironmentObject var store: MemoStore
+    
+    @State private var showComposer: Bool = false
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach (store.list) { memo in
+                    NavigationLink {
+                        DetailView(memo: memo)
+                    } label: {
+                        MemoCell(memo: memo)
+                    }
+                }
+                .onDelete(perform: store.delete)
+                
+            }
+            .listStyle(.plain)
+            .navigationTitle("권순엽의 메모장")
+            .toolbar{
+                Button {
+                    showComposer = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showComposer) {
+                ComposeView()
+        }
+
+        }
+        
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+            .environmentObject(MemoStore())
+    }
+}
+
